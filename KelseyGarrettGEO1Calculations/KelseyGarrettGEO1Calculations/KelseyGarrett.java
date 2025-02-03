1a87843180532b2d5985739a729c01f30a256db5
wuness <wondergud@gmail.com>
2025-02-03 15:06:48 -0800

    Create KelseyGarrett.java

---
+++ KelseyGarrettGEO1Calculations/KelseyGarrettGEO1Calculations/KelseyGarrett.java
@@ -0,0 +1,17 @@
+/*
+ *  Name: [Garrett Kelsey]
+ *  Class: CS1050 (T/TH)
+ *  Description: Guided Exploration 01 
+ *  This program will print a couple paragraphs about my passions and interests.       
+ *  The program will store and display my age and my aged squared. 
+ */
+
+package KelseyGarrettGEO1Calculations;
+
+/**
+ * 
+ */
+public class KelseyGarrett {
+//main
+//GEO1
+}
