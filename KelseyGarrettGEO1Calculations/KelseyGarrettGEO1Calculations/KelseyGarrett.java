b8d4e8719c6fa59cc9c6326d31dc928e195368ed
wuness <wondergud@gmail.com>
2025-02-03 16:46:31 -0800

    Grade Calculations Added
    Grade Calculations + Introductory paragraphs added under KelseyGarrett class

--- KelseyGarrettGEO1Calculations/KelseyGarrettGEO1Calculations/KelseyGarrett.java
+++ KelseyGarrettGEO1Calculations/KelseyGarrettGEO1Calculations/KelseyGarrett.java
@@ -7,11 +7,54 @@
  */
 
 package KelseyGarrettGEO1Calculations;
-
+// where to pull from
 /**
  * 
  */
 public class KelseyGarrett {
-//main
-//GEO1
+public static double  participation  = .10;
+public static double guided = .20;
+public static double quiz = .25;
+public static double iteration = .25;
+public static double ftest = .20;
+//
+
+
+// grade weight constants
+	
+	public static void main(String[] args) 	
+	{
+
+		System.out.println("Hello my name is Garrett! I am currently a junior at MSU Denver"); // line 1
+		System.out.println("I really enjoy listening to music it is my entire livelihood. I also quite enjoy very strange oddities.");
+		System.out.println("My goals for life is to eventually make a comfortable living in ethical hacking. I have always been obsessed with hacks in general.");
+		System.out.println("I am excited for what is to come in class! :)");
+		// introductory paragraph
+		
+		double participation2 = ((double)(80*participation));
+		double quiz2 = ((double)(90*quiz));
+		double guided2 = ((double)(70*guided));
+		double iteration2 = ((double)(85*iteration));
+		double ftest2 = ((double)(60*ftest));
+		// individual weighted final grades
+		System.out.println("Final Grade:");
+		System.out.println((participation2)+(quiz2)+(guided2)+(iteration2)+(ftest2));
+		// final grade
+		
+	//main
+	//GEO1
+	}
 }
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+	
+
