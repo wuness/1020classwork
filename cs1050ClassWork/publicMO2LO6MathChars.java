7dfb7c9a08795b28310daff8def4d93fda74e370
wuness <wondergud@gmail.com>
2025-02-11 12:16:29 -0800

    Create publicMO2LO6MathChars.java

---
+++ cs1050ClassWork/publicMO2LO6MathChars.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * 
+ */
+public class publicMO2LO6MathChars {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double exponentResult=Math.pow(10, 2);
+		System.out.println("result = " + exponentResult);
+		
+		double randomDouble= Math.random();
+		int randomint=(int)randomDouble;
+		
+		randomint=(int)(randomDouble * 10);
+		randomint=(int)(randomDouble * 10 + 1);
+		
+		char middleInitial = 'M';
+		// when declaring char use single quotes
+		int charInValue = 1;
+		System.out.println(middleInitial);
+		int charIntValue = (int)middleInitial;
+		System.out.println(charIntValue);
+		
+		// Assigns the character 4 to numCharacter
+		System.out.println("enter a char");
+		Scanner input = new Scanner(System.in);
+		char someChar = input.next().charAt(0);
+		System.out.println("A char has been entered in a Ascii value" + someChar);
+		
+		String myName = "Garrett";
+		// string use second capital due to it not being primitive data type
+		
+		String studentName = "Silly Willy";
+		String studentName1 = new String ("Willy Nilly");
+		String firstName = "Willy";
+		
+		
+		
+	}
+
+}
+
