b7c3d1500903fd93257256fa69308a72f517fe0e
wuness <wondergud@gmail.com>
2025-02-19 16:35:56 -0800

    changes (2/19/25)
    Added code (min and max values + input close) to GuessNumber after successful import.

--- guess/GuessNumber.java
+++ guess/GuessNumber.java
@@ -12,7 +12,7 @@ public class GuessNumber {
 	 */
 	public static void main(String[] args) {
 		// Main method
-	    int number = (int)(Math.random() * 101);
+	    int number = (int)(Math.random() * ((Math.pow(10, 2)+1)));
 
 	    Scanner input = new Scanner(System.in);
 	    System.out.println("Guess a magic number between 0 and 100");
@@ -22,6 +22,9 @@ public class GuessNumber {
 	      
 	      System.out.print("\nEnter your guess: ");
 	      guess = input.nextInt();
+	      
+	      if (guess < 0 && guess > 100);
+	        System.out.println("Your guess is out of the boundary 0-100");
 
 	      if (guess == number)
 	        System.out.println("Yes, the number is " + number);
@@ -29,6 +32,9 @@ public class GuessNumber {
 	        System.out.println("Your guess is too high");
 	      else
 	        System.out.println("Your guess is too low");
+	      
+	      
+	      input.close();
 	    } 
 		
 	}
