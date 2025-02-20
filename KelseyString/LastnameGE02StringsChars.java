e95520e9bafe97283091c03b07d8179321000a0e
wuness <wondergud@gmail.com>
2025-02-19 19:09:46 -0800

    changes.
    Saved name project and GEO.

--- KelseyString/LastnameGE02StringsChars.java
+++ KelseyString/LastnameGE02StringsChars.java
@@ -14,16 +14,22 @@ public class LastnameGE02StringsChars {
 		// TODO Auto-generated method stub
 	String studentName1 = "Stack Terror";
 	String lastName1 = "Terror";
+	char lastInitial1 = 'S';
+	char firstInitial1 = 'T';
+	int nameLength1 = studentName1.length();
 	// Naming student 1
 	
 	String studentName2 = "Memory Leak";
 	String lastName2 = "Leak";
+	char lastInitial2 = 'L';
+	char firstInitial2 = 'M';
+	int nameLength2 = studentName1.length();
 	// Naming student 2
 
-	System.out.println(studentName1.charAt(0) + "." + (lastName1.charAt(0) + ". " + (studentName1)));
+	System.out.println(studentName1.charAt(0) + "." + (lastName1.charAt(0) + ". " + (studentName1) + " (" + (nameLength1) + " 	characters long)"));
 	//Student 1 initials
 	
-	System.out.println(studentName2.charAt(0) + "." + (lastName2.charAt(0) + ". " + (studentName2)));
+	System.out.println(studentName2.charAt(0) + "." + (lastName2.charAt(0) + ". " + (studentName2) + " (" + (nameLength2) + " characters long)"));
 	//Student 2 initials
 	
 	System.out.println("Names in alphabetical order by last name:");
