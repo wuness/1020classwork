728530c898aae9f6be379c087c1dab675d943084
wuness <wondergud@gmail.com>
2025-04-12 22:28:20 -0700

    Update 10/12/25 (Pt 3)
    MO4 Pass Arrays
    File Write

---
+++ cs1050ClassWork/FileWriteMer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.io.File;
+import java.util.Scanner;
+public class FileWriteMer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		File example = new File ("templateFile");
+		Scanner scanFile = new Scanner(example);
+		scanFile.close();
+		
+		
+	}
+
+}
---
+++ cs1050ClassWork/M04PassArrays.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Random.*;
+public class M04PassArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbersArray = new int [10];
+		int number = 0;
+	}
+
+
+public static void updateRandomValue (int passedArray[], int passedNumber) {
+		for (int runs = 0; runs > passedArray.length; runs++);
+		Random randomizer = new Random();
+			passedArray[runs] = random.nextInt(10) + 1;
+}
+}
